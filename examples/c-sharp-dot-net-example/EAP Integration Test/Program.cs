22c59ea95f5530da74e905a8607901aa39d43613
Kevin Brown <kevin.brown@exogee.com>
2025-10-28 14:28:27 +1100

    Add C# .NET example for EAP API integration
    Introduces a .NET Core project demonstrating mutual TLS authentication and request to the EAP API. Includes example request payload, certificate handling utilities, server certificate validation, and setup instructions. Certificates are excluded from version control for security.

---
+++ examples/c-sharp-dot-net-example/EAP Integration Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using EAP_Integration_Test;
+
+X509Certificate2 clientCertificate = Utils.GetCertificateFromPEMPaths(Path.Join("certificates", "my-certificate.crt"), Path.Join("certificates", "my-private-key.key"));
+Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> ValidateServerCertificate = (sender, certificate, chain, sslPolicyErrors) => {
+    if (certificate == null) return false;
+    if (certificate.NotBefore > DateTime.Now || certificate.NotAfter < DateTime.Now)
+    {
+        Console.WriteLine("Server certificate is expired or not yet valid");
+        return false;
+    }
+
+    if (certificate.Subject != "CN=*.aiaapi.com") 
+    {
+        Console.WriteLine("Server certificate is not issued for *.aiaapi.com");
+        return false;
+    }
+
+    if (!certificate.Verify()) {
+        Console.WriteLine("Server certificate did not pass verify check.");
+        return false;
+    }
+
+    return true;
+};
+
+// Create an HttpClient with MTLS authentication
+HttpClient httpClient = new HttpClient(new HttpClientHandler
+{
+    ClientCertificateOptions = ClientCertificateOption.Manual,
+    SslProtocols = SslProtocols.Tls12,
+    ServerCertificateCustomValidationCallback = ValidateServerCertificate,
+    ClientCertificates = { clientCertificate },
+});
+httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+httpClient.DefaultRequestHeaders.Add("User-Agent", "EAP Integration Tester 1.0");
+
+// Send the API request
+HttpResponseMessage response = httpClient.PostAsync("https://emissionscalculator-mtls.production.aiaapi.com/calculator/v1/sheepbeef", ExampleRequest.content).Result;
+
+Console.WriteLine("Got Response:");
+Console.Write(response.Content.ReadAsStringAsync().Result);
---
+++ examples/c-sharp-dot-net-example/EAP Integration Test/certificates/README.md
@@ -0,0 +1,5 @@
+This directory should contain your certificate and private key.
+
+**Filenames**
+`my-certificate.crt`: The certificate file, starts with `-----BEGIN CERTIFICATE-----`.
+`my-private-key.key`: The key file, starts with `-----BEGIN PRIVATE KEY-----`.
