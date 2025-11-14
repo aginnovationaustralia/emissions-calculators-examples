08de1f3b90b2d9afd1d3470295e43fde0f2b5aee
Eddie Sholl <eddie.sholl@exogee.com>
2025-11-14 16:50:01 +1100

    Fully successful generation and execution of typescript client example

---
+++ examples/typescript-api-client/resources/setup_local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Setup script for local development and testing of the API client
+# This script creates a virtual environment and installs the client locally
+
+set -e
+
+npm install
